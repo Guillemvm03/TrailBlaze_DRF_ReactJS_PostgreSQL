d61aaa227a559c67c8b2e68c687200d96521228a
kevposesp <kevposesp>
2024-01-20 14:32:57 +0100

    wait for postgres

--- backend/django.sh
+++ backend/django.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+./wait-for-postgres.sh local_pgdb 5432
+
+until PGPASSWORD=$PG_PASSWORD psql -h $PG_HOST -U $PG_USER -lqt | cut -d \| -f 1 | grep -qw $PG_DB; do
+  echo "Waiting for the '$PG_DB' database to be created..."
+  sleep 1
+done
+
 echo "Showing Migrations..."
 python3 manage.py showmigrations
 echo ====================================
