d9e4bed0edc5e1b9ca7e1ce722be4cb65ac9fe09
Guillem <guillemvicent09@gmail.com>
2024-02-08 19:56:50 +0100

    first commit

--- frontend/src/context/StationsContext.jsx
+++ frontend/src/context/StationsContext.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import StationService from '../services/StationService';
 
+
 const Context = React.createContext({})
 
 export function StationContext({ children }) {
