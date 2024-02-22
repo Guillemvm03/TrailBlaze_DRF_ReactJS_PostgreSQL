44727f874ce02a126023a23b3193d838b2a06f14
kevposesp <kevposesp@gmail.com>
2024-02-22 16:32:53 +0100

    notification station

--- backend/trailblaze/app/notifications/serializers.py
+++ backend/trailblaze/app/notifications/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from .models import Notification
 from ..users.serializers import UserSerializer
+from ..stations.serializers import StationSerializer
 
 class NotificationSerializer(serializers.ModelSerializer):
     class Meta:
@@ -12,4 +13,5 @@ class NotificationSerializer(serializers.ModelSerializer):
         response['user'] = UserSerializer(instance.user).data
         response['to_user'] = UserSerializer(instance.to_user).data if instance.to_user else None
         response['response'] = NotificationSerializer(instance.response_notification).data if instance.response_notification else None
+        response['station'] = StationSerializer(instance.station).data if instance.station else None
         return response
--- frontend/src/pages/StationDetails/StationDetails.jsx
+++ frontend/src/pages/StationDetails/StationDetails.jsx
@@ -3,15 +3,34 @@ import { useNavigate } from "react-router-dom";
 import { useParams } from "react-router-dom";
 import { useStations } from "../../hooks/useStations";
 import SlotList from "../../components/client/Stations/Slots/SlotList";
+import { Button, Label, Card, TextInput } from 'flowbite-react';
+import Modal from '../../components/notifications/Modal';
+import { useNotifications } from "../../hooks/useNotifications";
 
 const StationDetails = () => {
   const navigate = useNavigate();
   const { slug } = useParams();
   const { useGetOneStation, oneStation } = useStations();
+  const { createNotification } = useNotifications();
+
+  const [title, setTitle] = useState('Problema');
+  const [description, setDescription] = useState('He tenido un problema con la bike');
+  const [status, setStatus] = useState('Active');
+  const [toUser, setToUser] = useState('');
 
   useEffect(function () {
     useGetOneStation(slug);
   }, []);
+
+  function create() {
+    const incidentData = {
+        title,
+        description,
+        status,
+        station: oneStation.id
+    };
+    createNotification(incidentData)
+}
 
   return (
     <div>
@@ -20,10 +39,51 @@ const StationDetails = () => {
       <br />
       <br />
       <h1 className="text-5xl font-extrabold dark:text-white">
-                 {oneStation.station_name} 
+        {oneStation.station_name}
         <small className="ms-2 font-semibold text-gray-500 dark:text-gray-400">
-                  \ {oneStation.address}
+          \ {oneStation.address}
         </small>
+        <Modal settings={{titleButton: "Create incident"}} sendData={() => create()}>
+          <div className="space-y-6 text-start mb-3">
+            <h3 className="text-xl font-medium text-gray-900 dark:text-white">Create a Incident</h3>
+            <div>
+              <div className="mb-2 block">
+                <Label htmlFor="title" value="Title" />
+              </div>
+              <TextInput
+                id="title"
+                placeholder="Incident Title"
+                value={title}
+                onChange={(event) => setTitle(event.target.value)}
+                required
+              />
+            </div>
+            <div>
+              <div className="mb-2 block">
+                <Label htmlFor="description" value="Description" />
+              </div>
+              <TextInput
+                id="description"
+                placeholder="Incident Description"
+                value={description}
+                onChange={(event) => setDescription(event.target.value)}
+                required
+              />
+            </div>
+            <div>
+              <div className="mb-2 block">
+                <Label htmlFor="status" value="Status" />
+              </div>
+              <TextInput
+                id="status"
+                placeholder="Incident Status"
+                value={status}
+                onChange={(event) => setStatus(event.target.value)}
+                required
+              />
+            </div>
+          </div>
+        </Modal>
         {/* {oneStation.id} */}
         <div className="flex items-center">
           <svg
@@ -79,16 +139,16 @@ const StationDetails = () => {
         role="status"
         className="space-y-8 md:space-y-0 md:space-x-8 rtl:space-x-reverse md:flex md:items-center"
       >
-        
+
         <div className="flex items-center justify-center w-full h-72 rounded sm:w-96">
-          <img className="w-96 h-72" src={oneStation.image} alt="image description"/>
+          <img className="w-96 h-72" src={oneStation.image} alt="image description" />
         </div>
 
 
 
         <div className="w-full">
           Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptas nulla totam aut cupiditate aliquid facilis repellat magni tempore a quasi iusto in rerum, distinctio perspiciatis praesentium sed nobis accusantium odit!
-        
+
         </div>
 
         <span className="sr-only">Loading...</span>
